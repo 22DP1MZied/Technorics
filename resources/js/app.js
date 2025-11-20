29d75e27e056324a1b571aa5d6948aee0509445f
22DP1MZied <mz00761@rvt.lv>
2025-11-20 10:47:24 +0200

    home page upgrade

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,14 @@
 import './bootstrap';
+document.querySelectorAll('.animate-bounce-slow').forEach(el => {
+    el.animate(
+        [
+            { transform: 'translateY(0px)' },
+            { transform: 'translateY(-20px)' },
+            { transform: 'translateY(0px)' }
+        ],
+        {
+            duration: 3000 + Math.random() * 2000,
+            iterations: Infinity
+        }
+    );
+});
